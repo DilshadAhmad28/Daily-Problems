8d529ff3f11bbec3b8f200b5166ad45377798a28
Dilshad Ahmad <146580057+DilshadAhmad28@users.noreply.github.com>
2025-08-24 18:38:59 +0530

    Time: 1 ms (34.07%), Space: 9.7 MB (18.66%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        int n=s.size();
+        unordered_map<char, int>mp;
+        for(auto ch : s){
+            mp[ch]++;
+        }
+        int c=0;
+        for(auto i : mp){
+            c=i.second;
+            mp.erase(i.first);
+            break;
+            
+        }
+        for(auto j : mp){
+            if(j.second!=c){
+                return false;
+            }
+        }
+        return true;
+    }
+};
