97c76b649d910b34b53ed26ac19036c9b0697881
Dilshad Ahmad <146580057+DilshadAhmad28@users.noreply.github.com>
2025-08-24 19:08:20 +0530

    Time: 0 ms (100%), Space: 9.6 MB (46.89%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool areOccurrencesEqual(string s) {
-        int n=s.size();
-        unordered_map<char, int>mp;
-        for(auto ch : s){
+      unordered_map<char, int> mp;
+        for (auto ch : s) {
             mp[ch]++;
         }
-        int c=mp[s[0]];
-        // for(auto i : mp){
-        //     c=i.second;
-        //     mp.erase(i.first);
-        //     break;
-            
-        // }
-
-        for(auto j : mp){
-            if(j.second!=c){
-                return false;
+        // reference frequency
+        int freq = mp.begin()->second;
+        for (auto j : mp) {
+            if (j.second != freq) {
+             return false;
             }
         }
         return true;
