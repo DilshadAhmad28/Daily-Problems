5ed03a96df4822548e64f0605713e2640b2d5ffe
Dilshad Ahmad <146580057+DilshadAhmad28@users.noreply.github.com>
2025-08-28 19:37:49 +0530

    Time: 0 ms (100%), Space: 9.5 MB (77.92%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n=s.size();
+        int i=0;
+        for(int i=0;i<n;i+=2*k){
+            int j=min(i + k - 1, n - 1);
+            reverse(s.begin() + i, s.begin() + j + 1);
+
+        }
+        return s;
+    }
+};
