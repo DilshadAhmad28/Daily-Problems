1fbd71e28eed9479d9e00cd68db8f27d9ec4aa73
Dilshad Ahmad <146580057+DilshadAhmad28@users.noreply.github.com>
2025-09-26 23:44:27 +0530

    Time: 9 ms (47.3%), Space: 65.1 MB (69.63%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,3 +1,18 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         unordered_set<int>st;
+//         for(int val:nums){
+//             if(st.find(val)!=st.end()){
+//                 return val;
+//             }
+//             st.insert(val);
+//         }
+//        return -1; 
+//     }
+// };
+
+//optimize approach
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
