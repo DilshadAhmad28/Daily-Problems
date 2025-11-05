129ed1c28a377a4dd221ade6dd299e93f918c871
Dilshad Ahmad <146580057+DilshadAhmad28@users.noreply.github.com>
2025-11-06 01:21:46 +0530

    Time: 0 ms (100%), Space: 32.2 MB (6.83%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, std::vector<int>& nums) {
+        int left = 0, right = 0;
+        int sum = 0;
+        int ans = std::numeric_limits<int>::max();
+        while (right < nums.size()) {
+            sum += nums[right];
+            while (sum >= target) {
+                ans = std::min(ans, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return (ans == std::numeric_limits<int>::max()) ? 0 : ans;
+    }
+};
